65bfce6fd5a42420b6370090b1be453f7161af92
VasayXTX <VasayXTX@mail.ru>
2011-04-12 20:20:56 +1100

    Add parsing without right bracket

--- src/dijkstra_rpn.py
+++ src/dijkstra_rpn.py
@@ -1,8 +1,9 @@
 import ply.lex as lex
+import re
 
 prog_name = "Dijkstra_RPN"
 
-class LexerException(Exception):
+class MyException(Exception):
     def __init__(self, pos, text):
         self.text = text
         self.pos = pos
@@ -38,7 +39,7 @@ class Lexer:
         return t
     
     def t_error(self, t):
-        raise LexerException(t.lexpos, "Illegal character")
+        raise MyException(t.lexpos, "Illegal character")
     
     def __init__(self):
         self.lexer = lex.lex(module=self)
@@ -48,19 +49,90 @@ class Lexer:
         
     def next(self):
         return self.lexer.token()
-
+    
+class Stack:    
+    def __init__(self):
+        self.s = []
+        
+    def is_empty(self):
+        return not len(self.s)
+    
+    def push(self, el):
+        self.s.append(el)
+    
+    def pop(self):
+        return self.s.pop()
+    
+    def back(self):
+        return self.s[len(self.s)-1]
+        
 class Parser:
     def __init__(self, lexer):
+        self.priority = [r'\(', r'\)', r'\+|-', r'\*|/', r'(um)|(up)']
         self.lexer = lexer
         
+    def get_priority(self, sym):
+        for i, el in enumerate(self.priority):
+            if re.search(el, sym): return i
+
+    def to_stack(self, sym):
+        pr = self.get_priority(sym)
+        while not self.stack.is_empty() and pr <= self.get_priority(self.stack.back()):
+            self.out.append(self.stack.pop())
+        self.stack.push(sym)
+                         
+    def parse_operand(self, t):
+        if t.type == 'NUMBER': 
+            self.out.append(t.value)
+            self.is_next_operand = False
+            return
+        elif t.type == 'LBRACKET':
+            self.stack.push(t.value)
+        elif t.type == 'MINUS':
+            self.to_stack('um')
+        elif t.type == 'PLUS':
+            self.to_stack('up')
+        else:
+            raise MyException(t.lexpos, "Invalid expression")
+        self.is_next_operand = True
+        
+    def parse_operation(self, t):
+        if t.type == 'PLUS':
+            self.to_stack('+')
+        elif t.type == 'MINUS':
+            self.to_stack('-')
+        if t.type == 'MUL':
+            self.to_stack('*')
+        if t.type == 'DIV':
+            self.to_stack('/')
+        self.is_next_operand = True
+            
     def parse(self, str):
         self.lexer.set_input(str)
-        arr = []
+        self.stack = Stack()
+        self.out = []
+        self.is_next_operand = True
         while True:
             t = self.lexer.next()
-            if not t: break
-            arr.append(t.value)
-        return arr
+            if not t:
+                while not self.stack.is_empty():
+                    self.out.append(self.stack.pop())
+                break
+            if self.is_next_operand:
+                self.parse_operand(t)
+            else:
+                self.parse_operation(t) 
+        return self.out
             
 p = Parser(Lexer())
-print(p.parse("1  +2 "))
+for line in open("input.in", "r"):
+    line = line.strip()
+    if not len(line): continue
+    try:
+        res = p.parse(line)
+    except MyException as e: 
+        print(prog_name + " " + e.msg)
+        continue
+    print(res)
+    
+#print(p.get_priority(r'u-'))
